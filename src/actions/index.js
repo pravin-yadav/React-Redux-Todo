5bf10269910f89a63a88ab7e8754eac4e8f9ac70
pravin-yadav <work.coderpravin@gmail.com>
2020-11-26 22:11:38 +0530

    Created async todo actions.

---
+++ src/actions/index.js
@@ -0,0 +1,29 @@
+import { CREATE_TODO, EDIT_TODO, DELETE_TODO, ACTIVE_TODO} from '../constants'
+
+export function createTodo(data) {
+    return {
+      type: CREATE_TODO,
+      payload: data
+    }
+  }
+  
+  export function editTodo(data) {
+    return {
+      type: EDIT_TODO,
+      payload: data
+    }
+  }
+  
+  export function deleteTodo(data) {
+    return {
+      type: DELETE_TODO,
+      payload: data
+    }
+  }
+
+  export function activeTodo(data) {
+    return {
+      type: ACTIVE_TODO,
+      payload: data
+    }
+  }
