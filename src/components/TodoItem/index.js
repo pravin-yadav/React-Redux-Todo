3ac094d8c3c7ba494ed0d45d092db95d06550844
pravin-yadav <work.coderpravin@gmail.com>
2020-11-29 18:24:50 +0530

    Added card user interface to the todo task elements.

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -10,21 +10,23 @@ const TodoItem = (props) => {
         dispatch(deleteTodo(todo))
     }    
     const todo = todos.map((todo, i) => 
-                <li key={todo.id + '_' + i} className="list-group-item d-flex justify-content-between my-2">
+                <div class="card mt-2 mb-2 pointer">
+                    <div class="card-body d-flex justify-content-between">
                     <div className="form-check">
                         <input className="form-check-input" type="checkbox" value="" id="defaultCheck1" />
                         <label className="form-check-label" htmlFor="defaultCheck1"></label>
                         <span className="ml-3 task-text text-dark">{todo.task}</span>
                     </div> 
                    <div className="todo-icon">
-                       <span onClick={() => handleEditTask(true, todo)} className="mx-4 text-secondary pointer" style={{fontSize: 28, cursor: 'pointer'}}>
+                       <span onClick={() => handleEditTask(true, todo)} className="mx-4 text-secondary pointer" style={{fontSize: 24, cursor: 'pointer'}}>
                             <i className="fa fa-pencil-square-o" aria-hidden="true"></i>
                        </span>
-                       <span onClick={() => handleDeleteTask(todo)} className="mx-2 text-danger pointer" style={{fontSize: 28, cursor: 'pointer'}}>
+                       <span onClick={() => handleDeleteTask(todo)} className="mx-2 text-danger pointer" style={{fontSize: 24, cursor: 'pointer'}}>
                             <i className="fa fa-trash-o" aria-hidden="true"></i>
                        </span>
                    </div>
-               </li>
+                    </div>
+                </div>
     )
         return(
             <React.Fragment>
--- src/components/TodoItemStatus/index.js
+++ src/components/TodoItemStatus/index.js
@@ -4,24 +4,28 @@ class TodoItemStatus extends React.Component{
     render(){
         return(
             <React.Fragment>
-                <li className="list-group-item d-flex justify-content-between my-2">
-                    <div>
-                        <i className="fa fa-dot-circle-o task-text" aria-hidden="true" style={{color: '#6c757d', fontSize: '18px'}}></i>
-                        <span className="ml-3 task-text">Performing a product search.</span>
-                    </div> 
-                   <div className="todo-icon">
-                        <span className="badge badge-info text-uppercase text-white" style={{fontSize: '12px'}}>IN PROGRESS</span>
-                   </div>
-               </li>
-                <li className="list-group-item d-flex justify-content-between my-2">
-                    <div>
-                        <i className="fa fa-dot-circle-o task-text" aria-hidden="true" style={{color: '#6c757d', fontSize: '18px'}}></i>
-                        <span className="ml-3 task-text" style={{textDecoration: 'line-through'}}>Adding to shopping cart.</span>
-                    </div> 
-                   <div className="todo-icon">
-                       <span className="badge badge-success text-uppercase text-white" style={{fontSize: '13px'}}>COMPLETED</span>
-                   </div>
-               </li>
+                <div class="card mt-2 mb-2 pointer">
+                    <div class="card-body d-flex justify-content-between">
+                        <div>
+                            <i className="fa fa-dot-circle-o task-text" aria-hidden="true" style={{color: '#6c757d', fontSize: '18px'}}></i>
+                            <span className="ml-3 task-text text-uppercase">Programming Tutorials</span>
+                        </div> 
+                        <div className="todo-icon">
+                                <span className="badge badge-info text-uppercase text-white" style={{fontSize: '12px'}}>IN PROGRESS</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mt-2 mb-2 pointer">
+                    <div class="card-body d-flex justify-content-between">
+                        <div>
+                            <i className="fa fa-dot-circle-o task-text" aria-hidden="true" style={{color: '#6c757d', fontSize: '18px'}}></i>
+                            <span className="ml-3 task-text" style={{textDecoration: 'line-through'}}>Adding to shopping cart.</span>
+                        </div> 
+                        <div className="todo-icon">
+                            <span className="badge badge-success text-uppercase text-white" style={{fontSize: '13px'}}>COMPLETED</span>
+                        </div>
+                    </div>
+               </div>
             </React.Fragment>
         )
     }
